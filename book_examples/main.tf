79391659a0ee24d19b3e2eea0312468ab547e1b4
Mahendra <mahendra.k12@gmail.com>
2020-08-19 16:06:27 +0530

    Add variables support

--- book_examples/main.tf
+++ book_examples/main.tf
@@ -15,7 +15,7 @@ resource "aws_instance" "vm_instance" {
   user_data = <<-EOF
   #!/bin/bash
   echo "Hello, World" > index.html
-  nohup busybox httpd -f -p 8080 &
+  nohup busybox httpd -f -p ${var.server_port} &
   EOF
 }
 
@@ -24,8 +24,8 @@ resource "aws_security_group" "instance_sg" {
   name = "terraform-example-instance"
 
   ingress {
-    from_port   = 8080
-    to_port     = 8080
+    from_port   = var.server_port
+    to_port     = var.server_port
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
