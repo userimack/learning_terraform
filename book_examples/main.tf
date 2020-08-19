9a3f2958b76d24bb4424812fdb72a1560edd4db9
Mahendra <mahendra.k12@gmail.com>
2020-08-19 17:51:45 +0530

    add launch config and autoscaling group

--- book_examples/main.tf
+++ book_examples/main.tf
@@ -2,21 +2,34 @@ provider "aws" {
   region = "us-east-2"
 }
 
-resource "aws_instance" "vm_instance" {
-  ami           = "ami-0c55b159cbfafe1f0"
-  instance_type = "t2.micro"
-
-  vpc_security_group_ids = [aws_security_group.instance_sg.id]
-
-  tags = {
-    Name = "terraform-example"
-  }
+resource "aws_launch_configuration" "alc_example" {
+  image_id        = "ami-0c55b159cbfafe1f0"
+  instance_type   = "t2.micro"
+  security_groups = [aws_security_group.instance_sg.id]
 
   user_data = <<-EOF
   #!/bin/bash
   echo "Hello, World" > index.html
   nohup busybox httpd -f -p ${var.server_port} &
   EOF
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_autoscaling_group" "asg_example" {
+  launch_configuration = aws_launch_configuration.alc_example.name
+  vpc_zone_identifier  = data.aws_subnet_ids.default.ids
+
+  min_size = 2
+  max_size = 4
+
+  tag {
+    key                 = "Name"
+    value               = "terraform-asg-example"
+    propagate_at_launch = true
+  }
 }
 
 
@@ -31,3 +44,10 @@ resource "aws_security_group" "instance_sg" {
   }
 }
 
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnet_ids" "default" {
+  vpc_id = data.aws_vpc.default.id
+}
--- book_examples/outputs.tf
+++ book_examples/outputs.tf
@@ -1,4 +1,4 @@
-output "public_ip" {
-  value       = aws_instance.vm_instance.public_ip
-  description = "The public IP address of the web server"
-}
+# output "public_ip" {
+#   value       = aws_instance.vm_instance.public_ip
+#   description = "The public IP address of the web server"
+# }
