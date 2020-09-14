cb10cf5fc3f1b387951b663cfbe81a9a07ee772d
Mahendra <mahendra.k12@gmail.com>
2020-09-14 07:44:10 +0530

    update mysql and hello-world-app module for terragrunt

--- book_examples/modules/services/hello-world-app/main.tf
+++ book_examples/modules/services/hello-world-app/main.tf
@@ -1,5 +1,15 @@
+provider "aws" {
+  region = "us-east-2"
+
+  # Allow any 2.x version of the AWS provider
+  version = "~> 3.3"
+}
+
 terraform {
   required_version = ">= 0.13, < 0.14"
+
+  # Partial configuration. The rest will be filled in by Terragrunt.
+  backend "s3" {}
 }
 
 module "asg" {
